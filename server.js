c3696c01c5e38214666c6efba4eda8212e0dc2ef
malwareami! <46420533+malicious-dev@users.noreply.github.com>
2022-12-09 02:31:12 +0530

    c

--- server.js
+++ server.js
@@ -4,4 +4,9 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 const app = express();
-const port = proce
+const port = process.env.PORT || 3000;
+app.use(cors());
+
+app.listen(port, () => {
+  console.log('listening on port ' + port)
+});
