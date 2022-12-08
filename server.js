340c944f0db4785f5e6bbf6b8287daf089824728
malwareami! <46420533+malicious-dev@users.noreply.github.com>
2022-12-09 02:30:43 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const app = express();
+const port = proce
